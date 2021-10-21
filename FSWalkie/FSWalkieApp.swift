38f8e6a0c0bead1986cf67c5646bcd10910a6a28
Rayen Kamta <mr.rayenk@gmail.com>
2021-10-21 12:20:52 -0400

    Initial Commit

---
+++ FSWalkie/FSWalkieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FSWalkieApp.swift
+//  FSWalkie
+//
+//  Created by Rayen Kamta on 10/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FSWalkieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
